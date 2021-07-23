7ce9285782d1b624ec368e4a8ac70261686f66fd
basebandit <evansonmwangi83@gmail.com>
2021-07-23 14:03:54 +0300

    Add icon and add reminder styling dynamically

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,14 @@
-const Task = ({ task }) => {
+import { FaTimes } from "react-icons/fa";
+
+const Task = ({ task, onToggle }) => {
   return (
-    <div className="task">
-      <h3>{task.text}</h3>
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} />
+      </h3>
       <p>{task.day}</p>
     </div>
   );
