cd7f92ff66faeb6236d203e63b4df37e6050d3c8
basebandit <evansonmwangi83@gmail.com>
2021-07-23 13:07:02 +0300

    Pass tasks state down to tasks component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import "./App.css";
 import Tasks from "./components/Tasks";
 import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2.30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: "Learn react basics",
+      day: "July 20th at 2:00pm",
+      reminder: true,
+    },
+  ]);
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} onToggle={toggleReminder} />
     </div>
   );
 }
