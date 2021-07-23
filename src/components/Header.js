6123995640329574b5420840e116c7a966162800
basebandit <evansonmwangi83@gmail.com>
2021-07-23 12:46:51 +0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = (props) => {
+  return (
+    <div>
+      <header className="header">
+        <h1>{props.title}</h1>
+        <Button text="Add" />
+      </header>
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
