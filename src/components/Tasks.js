8c41f5aaf5ef5730c04e6934cd0a453e4bb9a994
basebandit <evansonmwangi83@gmail.com>
2021-07-27 14:18:25 +0300

    only show tasks if there are any

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,14 +3,16 @@ import Task from "./Task";
 const Tasks = ({ tasks, onToggle, onDelete }) => {
   return (
     <>
-      {tasks.map((task) => (
-        <Task
-          key={task.id}
-          task={task}
-          onToggle={onToggle}
-          onDelete={onDelete}
-        />
-      ))}
+      {tasks.length > 0
+        ? tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              onToggle={onToggle}
+              onDelete={onDelete}
+            />
+          ))
+        : "No Task To Show"}
     </>
   );
 };
