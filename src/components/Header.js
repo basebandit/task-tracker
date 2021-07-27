86421d807b0f55966497ab345402ea25bac64500
basebandit <evansonmwangi83@gmail.com>
2021-07-27 18:01:59 +0300

    show button conditionally on location path name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, showAdd, onAdd }) => {
+  const location = useLocation();
+
   return (
     <div>
       <header className="header">
         <h1>{title}</h1>
-        <Button
-          color={showAdd ? "red" : "green"}
-          text={showAdd ? "Close" : "Add"}
-          onClick={onAdd}
-        />
+        {location.pathname === "/" && (
+          <Button
+            color={showAdd ? "red" : "green"}
+            text={showAdd ? "Close" : "Add"}
+            onClick={onAdd}
+          />
+        )}
       </header>
     </div>
   );
